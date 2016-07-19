GoogleCloudPlatform/app-gradle-plugin

commit 51a9e4c3a5320f101cf3ea91f112970bb18dfcab

Run dev appserver from exploded-war directory (#47)
* Run dev appserver from exploded-war directory

src/main/java/com/google/cloud/tools/gradle/appengine/AppEngineStandardPlugin.java
@@ -124,7 +124,7 @@ public class AppEngineStandardPlugin implements Plugin<Project> {
       app.getStage().setStagingDirectory(new File(buildDir, STAGED_APP_DIR_NAME));
 
       List<File> deployables = Collections
-          .singletonList(new File(app.getStage().getStagingDirectory(), "app.yaml"));
+          .singletonList(new File(buildDir, EXPLODED_APP_DIR_NAME));
       app.getRun().setAppYamls(deployables);
       app.getDeploy().setDeployables(deployables);
 
@@ -187,7 +187,7 @@ public class AppEngineStandardPlugin implements Plugin<Project> {
           runTask.setCloudSdkBuilderFactory(factory);
           runTask.setGroup(APP_ENGINE_STANDARD_TASK_GROUP);
           runTask.setDescription("Run an App Engine standard environment application locally");
-          runTask.dependsOn(STAGE_TASK_NAME);
+          runTask.dependsOn(BasePlugin.ASSEMBLE_TASK_NAME);
         }
       });
 
@@ -198,7 +198,7 @@ public class AppEngineStandardPlugin implements Plugin<Project> {
           startTask.setCloudSdkBuilderFactory(factory);
           startTask.setGroup(APP_ENGINE_STANDARD_TASK_GROUP);
           startTask.setDescription("Run an App Engine standard environment application locally in the background");
-          startTask.dependsOn(STAGE_TASK_NAME);
+          startTask.dependsOn(BasePlugin.ASSEMBLE_TASK_NAME);
         }
       });
 

src/test/java/com/google/cloud/tools/gradle/appengine/AppEngineStandardPluginTest.java
@@ -17,6 +17,8 @@
 
 package com.google.cloud.tools.gradle.appengine;
 
+import com.google.common.base.Charsets;
+
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
 import org.junit.Assert;
@@ -25,6 +27,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -50,7 +53,8 @@ public class AppEngineStandardPluginTest {
 
     Path webInf = testProjectDir.getRoot().toPath().resolve("src/main/webapp/WEB-INF");
     Files.createDirectories(webInf);
-    Files.createFile(webInf.resolve("appengine-web.xml"));
+    File appengineWebXml = Files.createFile(webInf.resolve("appengine-web.xml")).toFile();
+    Files.write(appengineWebXml.toPath(), "<appengine-web-app/>".getBytes(Charsets.UTF_8));
   }
 
   @Test
@@ -78,7 +82,7 @@ public class AppEngineStandardPluginTest {
 
     final List<String> expected = Arrays
         .asList(":compileJava", ":processResources", ":classes", ":war", ":explodeWar", ":assemble",
-            ":appengineStage", ":appengineRun");
+            ":appengineRun");
 
     Assert.assertEquals(expected, BuildResultFilter.extractTasks(buildResult));
   }
@@ -93,7 +97,7 @@ public class AppEngineStandardPluginTest {
 
     final List<String> expected = Arrays
         .asList(":compileJava", ":processResources", ":classes", ":war", ":explodeWar", ":assemble",
-            ":appengineStage", ":appengineStart");
+            ":appengineStart");
 
     Assert.assertEquals(expected, BuildResultFilter.extractTasks(buildResult));
 

commit 183be1bc0ad57b5a7df56302553c2d55778ca93e

Fix deployables to deploy app.yaml from staged
Issues introduced by #47

src/main/java/com/google/cloud/tools/gradle/appengine/AppEngineStandardPlugin.java
@@ -124,9 +124,10 @@ public class AppEngineStandardPlugin implements Plugin<Project> {
       app.getStage().setSourceDirectory(new File(buildDir, EXPLODED_APP_DIR_NAME));
       app.getStage().setStagingDirectory(new File(buildDir, STAGED_APP_DIR_NAME));
 
+      List<File> runnables = Collections.singletonList(new File(buildDir, EXPLODED_APP_DIR_NAME));
+      app.getRun().setAppYamls(runnables);
       List<File> deployables = Collections
-          .singletonList(new File(buildDir, EXPLODED_APP_DIR_NAME));
-      app.getRun().setAppYamls(deployables);
+          .singletonList(new File(app.getStage().getStagingDirectory(), "app.yaml"));
       app.getDeploy().setDeployables(deployables);
 
       StandardDataExtension projectData = extension.getByType(StandardDataExtension.class);
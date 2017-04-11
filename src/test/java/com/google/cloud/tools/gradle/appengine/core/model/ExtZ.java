94baadb79bdb6571889aab2319f8cbc2d2cc102c
Appu Goundan <appu@google.com>
2017-04-11 12:27:11 -0400

    Refactor packaging, move extension defaults to plugin init

---
+++ src/test/java/com/google/cloud/tools/gradle/appengine/core/model/ExtZ.java
@@ -0,0 +1,12 @@
+package com.google.cloud.tools.gradle.appengine.core.model;
+
+import com.google.common.collect.ImmutableMap;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class ExtZ {
+  private String zz = "hello";
+  private Map<String, List<String>> zzNested =
+      ImmutableMap.of("a", Arrays.asList("a1", "a2"), "b", Arrays.asList("b1", "b2"));
+}

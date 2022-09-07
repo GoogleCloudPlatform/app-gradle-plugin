034653fe0f7a53ae04bdad79b5a905ffd315af08
Elena Felder <41136058+elefeint@users.noreply.github.com>
2022-09-07 09:16:49 -0400

    Update copyright.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.util.Date
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 
 /*
- * Copyright 2017 Google LLC. All Rights Reserved.
+ * Copyright 2022 Google LLC. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

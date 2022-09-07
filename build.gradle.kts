fe5d5d810a890452b0bfc0453349a7a3c8da93a6
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2022-09-07 17:24:50 +0300

    style: build.gradle.kts, fix indentation in build.gradle.kts (official Kotlin guide is to use 4 spaces)

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,195 +20,198 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
  */
 
 plugins {
-  id("java")
-  id("maven")
-  id("java-gradle-plugin")
-  id("net.researchgate.release") version "2.6.0"
-  id("com.github.sherter.google-java-format") version "0.8"
-  id("checkstyle")
-  id("jacoco")
+    id("java")
+    id("maven")
+    id("java-gradle-plugin")
+    id("net.researchgate.release") version "2.6.0"
+    id("com.github.sherter.google-java-format") version "0.8"
+    id("checkstyle")
+    id("jacoco")
 }
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 group = "com.google.cloud.tools"
 
 dependencies {
-  compile(localGroovy())
-  compile(gradleApi())
-  compile("com.google.cloud.tools:appengine-plugins-core:0.9.9")
+    compile(localGroovy())
+    compile(gradleApi())
+    compile("com.google.cloud.tools:appengine-plugins-core:0.9.9")
 
-  testCompile("commons-io:commons-io:2.4")
-  testCompile("junit:junit:4.12")
-  testCompile("org.hamcrest:hamcrest-library:1.3")
-  testCompile("org.mockito:mockito-core:2.23.4")
+    testCompile("commons-io:commons-io:2.4")
+    testCompile("junit:junit:4.12")
+    testCompile("org.hamcrest:hamcrest-library:1.3")
+    testCompile("org.mockito:mockito-core:2.23.4")
 }
 
 
 tasks.wrapper {
-  gradleVersion = "6.9"
+    gradleVersion = "6.9"
 }
 
 tasks.jar.configure {
-  manifest {
-    attributes(
-      mapOf(
-        "Implementation-Title" to project.name,
-        "Implementation-Version" to project.version,
-        "Built-By" to System.getProperty("user.name"),
-        "Built-Date" to Date(),
-        "Built-JDK" to System.getProperty("java.version"),
-        "Built-Gradle" to gradle.gradleVersion
-      )
-    )
-  }
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version,
+                "Built-By" to System.getProperty("user.name"),
+                "Built-Date" to Date(),
+                "Built-JDK" to System.getProperty("java.version"),
+                "Built-Gradle" to gradle.gradleVersion
+            )
+        )
+    }
 }
 
 /* TESTING */
 tasks.test.configure {
-  testLogging {
-    showStandardStreams = true
-    exceptionFormat = FULL
-  }
+    testLogging {
+        showStandardStreams = true
+        exceptionFormat = FULL
+    }
 }
 
 sourceSets {
-  create("integTest") {
-    compileClasspath += main.get().output
-    runtimeClasspath += main.get().output
-  }
+    create("integTest") {
+        compileClasspath += main.get().output
+        runtimeClasspath += main.get().output
+    }
 }
 
 configurations {
-  named("integTestCompile").get().extendsFrom(testCompileClasspath.get())
-  named("integTestRuntime").get().extendsFrom(testRuntimeClasspath.get())
+    named("integTestCompile").get().extendsFrom(testCompileClasspath.get())
+    named("integTestRuntime").get().extendsFrom(testRuntimeClasspath.get())
 }
 
 tasks.register<Test>("integTest") {
-  testClassesDirs = sourceSets.getByName("integTest").output.classesDirs
-  classpath = sourceSets.getByName("integTest").runtimeClasspath
-  outputs.upToDateWhen { false }
+    testClassesDirs = sourceSets.getByName("integTest").output.classesDirs
+    classpath = sourceSets.getByName("integTest").runtimeClasspath
+    outputs.upToDateWhen { false }
 }
 /* TESTING */
 
 
 /* RELEASING */
 tasks.register<Jar>("sourceJar") {
-  from(sourceSets.main.get().allJava)
-  classifier = "sources"
+    from(sourceSets.main.get().allJava)
+    classifier = "sources"
 }
 
 tasks.register<Jar>("javadocJar") {
-  dependsOn(tasks.javadoc)
-  from(tasks.javadoc.map { it.destinationDir!! })
-  classifier = "javadoc"
+    dependsOn(tasks.javadoc)
+    from(tasks.javadoc.map { it.destinationDir!! })
+    classifier = "javadoc"
 }
 
 project.afterEvaluate {
-  tasks.register("writePom") {
-    val outputFile = file("$buildDir/pom/${project.name}-${project.version}.pom")
-    outputs.file(outputFile)
+    tasks.register("writePom") {
+        val outputFile = file("$buildDir/pom/${project.name}-${project.version}.pom")
+        outputs.file(outputFile)
 
-    doLast {
-      maven {
-        pom {
-          project {
-            withGroovyBuilder {
-              "name"("App Engine Gradle Plugin")
-              "description"("This Gradle plugin provides tasks to build and deploy Google App Engine applications.")
+        doLast {
+            maven {
+                pom {
+                    project {
+                        withGroovyBuilder {
+                            "name"("App Engine Gradle Plugin")
+                            "description"("This Gradle plugin provides tasks to build and deploy Google App Engine applications.")
 
-              "url"("https://github.com/GoogleCloudPlatform/app-gradle-plugin")
-              "inceptionYear"("2016")
+                            "url"("https://github.com/GoogleCloudPlatform/app-gradle-plugin")
+                            "inceptionYear"("2016")
 
-              "scm" {
-                "url"("https://github.com/GoogleCloudPlatform/app-gradle-plugin")
-                "connection"("scm:https://github.com/GoogleCloudPlatform/app-gradle-plugin.git")
-                "developerConnection"("scm:git://github.com/GoogleCloudPlatform/app-gradle-plugin.git")
-              }
+                            "scm" {
+                                "url"("https://github.com/GoogleCloudPlatform/app-gradle-plugin")
+                                "connection"("scm:https://github.com/GoogleCloudPlatform/app-gradle-plugin.git")
+                                "developerConnection"("scm:git://github.com/GoogleCloudPlatform/app-gradle-plugin.git")
+                            }
 
-              "licenses" {
-                "license" {
-                  "name"("The Apache Software License, Version 2.0")
-                  "url"("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                  "distribution"("repo")
-                }
-              }
-              "developers" {
-                "developer" {
-                  "id"("loosebazooka")
-                  "name"("Appu Goundan")
-                  "email"("appu@google.com")
-                }
-              }
+                            "licenses" {
+                                "license" {
+                                    "name"("The Apache Software License, Version 2.0")
+                                    "url"("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                                    "distribution"("repo")
+                                }
+                            }
+                            "developers" {
+                                "developer" {
+                                    "id"("loosebazooka")
+                                    "name"("Appu Goundan")
+                                    "email"("appu@google.com")
+                                }
+                            }
+                        }
+                    }
+                }.writeTo(outputFile)
             }
-          }
-        }.writeTo(outputFile)
-      }
+        }
     }
-  }
 }
 
 
 // for kokoro releases
 tasks.register<Sync>("prepareRelease") {
-  from(tasks.jar)
-  from(tasks.named("sourceJar"))
-  from(tasks.named("javadocJar"))
-  from(tasks.named("writePom"))
+    from(tasks.jar)
+    from(tasks.named("sourceJar"))
+    from(tasks.named("javadocJar"))
+    from(tasks.named("writePom"))
 
-  into("${buildDir}/release-artifacts")
+    into("${buildDir}/release-artifacts")
 
-  dependsOn(tasks.build)
+    dependsOn(tasks.build)
 }
 
 release {
-  tagTemplate = "v\$version"
-  getProperty("git").apply {
-    this as GitConfig
-    requireBranch = """^release-v\d+.*$"""  //regex
-  }
+    tagTemplate = "v\$version"
+    getProperty("git").apply {
+        this as GitConfig
+        requireBranch = """^release-v\d+.*$"""  //regex
+    }
 }
 /* RELEASING */
 
 /* FORMATTING */
 googleJavaFormat {
-  toolVersion = "1.7"
+    toolVersion = "1.7"
 }
 
 
 tasks.check.configure {
-  dependsOn(tasks.verifyGoogleJavaFormat)
+    dependsOn(tasks.verifyGoogleJavaFormat)
 }
 // to auto-format run ./gradlew googleJavaFormat
 
 checkstyle {
-  toolVersion = "8.18"
-  // get the google_checks.xml file from the actual tool we"re invoking)
-  config = resources.text.fromArchiveEntry(configurations.checkstyle.files.first(), "google_checks.xml")
-  maxErrors = 0
-  maxWarnings = 0
-  tasks.checkstyleTest.configure {
-    enabled = false
-  }
+    toolVersion = "8.18"
+    // get the google_checks.xml file from the actual tool we"re invoking)
+    config = resources.text.fromArchiveEntry(
+        configurations.checkstyle.files.first(),
+        "google_checks.xml"
+    )
+    maxErrors = 0
+    maxWarnings = 0
+    tasks.checkstyleTest.configure {
+        enabled = false
+    }
 }
 /* FORMATTING */
 
 /* TEST COVERAGE */
 jacoco {
-  toolVersion = "0.8.6"
+    toolVersion = "0.8.6"
 }
 
 tasks.jacocoTestReport {
-  reports {
-    xml.isEnabled = true
-    html.isEnabled = false
-  }
+    reports {
+        xml.isEnabled = true
+        html.isEnabled = false
+    }
 }
 /* TEST COVERAGE */

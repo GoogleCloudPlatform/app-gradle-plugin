6dd07b0880b33ad7e79e67c7362f712107f3196a
Diego Alonso Marquez Palacios <diegomarquezp@google.com>
2023-07-04 15:42:49 -0400

    chore: update java version for kokoro windows
    New `win2019` image uses `jdk1.8.0_211`

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -1,7 +1,7 @@
 @echo on
 
 REM Java 9 does not work with our build at the moment, so force java 8
-set JAVA_HOME=c:\program files\java\jdk1.8.0_152
+set JAVA_HOME=c:\program files\java\jdk1.8.0_211
 set PATH=%JAVA_HOME%\bin;%PATH%
 
 cd github/app-gradle-plugin
